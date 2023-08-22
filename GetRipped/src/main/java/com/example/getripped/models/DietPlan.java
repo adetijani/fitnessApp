9bfe2c087af2cdc69dd9e8e7dbe2ec2eac651ece
Decagon <deca@Decagons-MacBook-Air.local>
2023-08-22 23:02:08 +0100

    diet

---
+++ GetRipped/src/main/java/com/example/getripped/models/DietPlan.java
@@ -0,0 +1,19 @@
+package com.example.getripped.models;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+
+public class DietPlan extends PlanBaseEntity{
+    private String diet;
+
+    private Long mentorId;
+
+
+}
--- GetRipped/src/main/java/com/example/getripped/models/PlanBaseEntity.java
+++ GetRipped/src/main/java/com/example/getripped/models/PlanBaseEntity.java
@@ -1,5 +1,8 @@
 package com.example.getripped.models;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.AllArgsConstructor;
 import lombok.Data;
